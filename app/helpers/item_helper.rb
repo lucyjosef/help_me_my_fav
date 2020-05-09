6a96620a2099743bf062bd8fd782b7f1da51eaac
lucyjosef <luciejoseph@ymail.com>
2020-05-09 13:07:02 +0200

    instanciated item helper for scrap

--- app/helpers/item_helper.rb
+++ app/helpers/item_helper.rb
@@ -1,2 +1,13 @@
 module ItemHelper
+    # Keep it commented cause it fails :/ 
+    # require 'HTTParty'
+    # require 'Nokogiri'
+
+    def rescrap(item)
+    end
+
+    def scrap_new(item)
+        doc = HTTParty.get(item.link)
+        @parsed_page ||= Nokogiri::HTML(doc)
+    end
 end
